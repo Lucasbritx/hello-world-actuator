bf3d3fb49093d053d947c055ae79ba792c40db9e
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-05-20 21:47:34 -0300

    Initial commit

---
+++ src/main/java/com/example/helloworldactuator/HelloWorldActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloworldactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldActuatorApplication.class, args);
+	}
+
+}
